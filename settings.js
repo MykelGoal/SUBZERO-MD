a4a85b686e8c8ba92de821cf49dca213e057d41e
MykelGoal <gabrielbamidele415@gmail.com>
2025-07-19 21:34:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;eZUmgLqJ#kcqNAC74mfZ3hNNstoV4APS7BlJrCk5kAPKla4k0ysc", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
